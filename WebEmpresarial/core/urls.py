11a6015b1a9c10a60682b345c1ee982fb7c1d676
Dragonf-j <jmjejuan@gmail.com>
2023-12-03 13:18:53 +0100

    created app services

--- WebEmpresarial/core/urls.py
+++ WebEmpresarial/core/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     #core path
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
-    path('services/', views.services, name="servicios"),
     path('store/', views.store, name="tienda"),
     path('contac/', views.contact, name="contacto"),
     path('blog/', views.blog, name="blog"),
---
+++ WebEmpresarial/service/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Service(models.Model):
+    tittle = models.CharField(max_length=200, verbose_name='Titulo')
+    subtittle = models.CharField(max_length=200, verbose_name='Subtitulo')
+    context = models.TextField(verbose_name='Contenido')
+    image = models.ImageField(verbose_name='imagen', upload_to='services')
+    created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creacion')
+    update = models.DateTimeField(auto_now=True, verbose_name='Fecha de modificacion')
+
+    class Meta:
+        verbose_name = 'servicio'
+        verbose_name_plural = 'servicios'
+        ordering = ['-created']
+    def __str__(self):
+        return self.tittle
