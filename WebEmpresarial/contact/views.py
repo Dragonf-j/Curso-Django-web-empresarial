eb1939d6ba9e5f224bb6091f906452be4c0bea3a
Dragonf-j <55980623+Dragonf-j@users.noreply.github.com>
2024-01-23 22:34:21 +0100

    Procesamiento y validadcion del formulario

--- WebEmpresarial/contact/views.py
+++ WebEmpresarial/contact/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .forms import ContactForm
 # Create your views here.
 def contact(request):
+    #print('Tipo de peticion: {}'.format(request.method))
     contactForm = ContactForm()
+    if request.method == 'POST':
+        contactForm = ContactForm(data=request.POST)
+        if contactForm.is_valid():
+            name= request.POST.get('name', '')
+            email= request.POST.get('email', '')
+            content= request.POST.get('content', '')
+            #sopones que todo fue bien y redicreccionamos
+            return redirect(reverse('contacto')+"?ok") #tiene que ser el nombre que esta en urls
+
     return render(request,"contact/contact.html", {'form':contactForm})
