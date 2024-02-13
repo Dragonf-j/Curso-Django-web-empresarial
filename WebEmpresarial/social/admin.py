da9c5a31b7304c342e3cf59239850b4af51aca04
Dragonf-j <55980623+Dragonf-j@users.noreply.github.com>
2024-02-13 22:25:28 +0100

    Finish commit
    Ultimo commit de este proyecto de practica para el curso de Django

--- WebEmpresarial/social/admin.py
+++ WebEmpresarial/social/admin.py
@@ -1,8 +1,16 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
 from .models import Link
 
 # Register your models here.
 
 class LinkAdmin(admin.ModelAdmin):
     readonly_fields =  ('created', 'update')
+
+    def get_readonly_fields(self, request, obj = None):
+        if request.user.groups.filter(name="personal"):
+            return ('created', 'update', 'key', 'name')
+        else:
+            return ('created', 'update')
 admin.site.register(Link, LinkAdmin)
